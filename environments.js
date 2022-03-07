46a4698ad499e5c78944ac1595d1781c54a8c5a2
Mischa <mischa@mmt.me.uk>
2022-03-07 23:12:32 +0000

    This changes the way we do config. Enabling deployment to Ropsten with Github Action

--- environments.js
+++ environments.js
@@ -4,14 +4,17 @@
  *  - Copy to environments.js and edit to suit local needs.
  *  - environments.js is in .gitignore and will not be committed
  */
+
+require('dotenv').config()
+
 module.exports = {
 
     // For helping public deployments succeed if default gas doesn't work
-    "gasLimit": 20450000,
+    "gasLimit": parseInt(process.env.DEPLOYER_GAS_LIMIT),
 
     // Needed for verifying contract code on Etherscan
     "etherscan": {
-        "apiKey": ""
+        "apiKey": process.env.ETHERSCAN_API_KEY,
     },
 
     // Needed for Gas Reporter
@@ -19,28 +22,26 @@ module.exports = {
         "apiKey": ""
     },
 
-
     // Hardhat testnet
     //  - throwaway HDWallet mnemonic for running unit tests, which require more than one address
     "hardhat": {
-        "txNode": "",
-        "mnemonic": "reduce chair insane vault universe fitness flame motor wood toy vacuum special"
+        "mnemonic": process.env.DEPLOYER_HARDHAT_MNEMONIC
     },
 
     // Ropsten testnet
     //  - placeholder private key is first address of test HDWallet used in hardhat network config
     //  - Replace key with multisig pk for deployment
     "ropsten": {
-        "txNode": "",
-        "keys": ["0xb644aa4a6ada1f1a996def5cee46b93922006d78754095525cefeed36f5ab1c0"]
+        "txNode": process.env.DEPLOYER_ROPSTEN_TXNODE,
+        "keys": [process.env.DEPLOYER_ROPSTEN_KEY]
     },
 
     // Ethereum Mainnet
     //  - placeholder private key is first address of test HDWallet used in hardhat network config
     //  - Replace key with multisig pk for deployment
     "mainnet": {
-        "txNode": "",
-        "keys": ["0xb644aa4a6ada1f1a996def5cee46b93922006d78754095525cefeed36f5ab1c0"]
+        "txNode": process.env.DEPLOYER_MAINNET_TXNODE,
+        "keys": [process.env.DEPLOYER_MAINNET_KEY]
     }
 
 };
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,6 @@
+const dotEnvConfig = require('dotenv');
+dotEnvConfig.config();
+
 const environments = require('./environments');
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
@@ -6,51 +9,52 @@ require('hardhat-contract-sizer');
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
+
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      accounts: {mnemonic: environments.hardhat.mnemonic},
-      gas: environments.gasLimit
-    },
-    ropsten: {
-      url: environments.ropsten.txNode,
-      accounts: environments.ropsten.keys,
-      gas: environments.gasLimit
-    },
-    mainnet: {
-      url: environments.mainnet.txNode,
-      accounts: environments.mainnet.keys,
-      gas: environments.gasLimit
-    }
-  },
-  etherscan: {
-    apiKey: environments.etherscan.apiKey
-  },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-        details: {
-          yul: true
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            accounts: {mnemonic: environments.hardhat.mnemonic},
+            gas: environments.gasLimit
+        },
+        ropsten: {
+            url: environments.ropsten.txNode,
+            accounts: environments.ropsten.keys,
+            gas: environments.gasLimit
+        },
+        mainnet: {
+            url: environments.mainnet.txNode,
+            accounts: environments.mainnet.keys,
+            gas: environments.gasLimit
         }
-      }
+    },
+    etherscan: {
+        apiKey: environments.etherscan.apiKey
+    },
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+                details: {
+                    yul: true
+                }
+            }
+        }
+    },
+    gasReporter: {
+        currency: 'USD',
+        enabled: true,
+        gasPrice: 300,
+        coinmarketcap: environments.coinmarketcap.apiKey,
+        showTimeSpent: true,
+        showMethodSig: false
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
     }
-  },
-  gasReporter: {
-    currency: 'USD',
-    enabled: true,
-    gasPrice: 300,
-    coinmarketcap: environments.coinmarketcap.apiKey,
-    showTimeSpent: true,
-    showMethodSig: false
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  }
 };
