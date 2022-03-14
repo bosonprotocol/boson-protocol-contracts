43a907e179f1ba4f771f5463b328b634d376c1c7
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2022-03-14 16:52:27 +0530

    Condition - EvaluationMethod domain related changes

---
+++ scripts/domain/EvaluationMethod.js
@@ -0,0 +1,26 @@
+/**
+ * Boson Protocol Domain Enum: EvaluationMethod
+ */
+const NODE = (typeof module !== 'undefined' && typeof module.exports !== 'undefined');
+class EvaluationMethod {}
+
+EvaluationMethod.None = 0;
+EvaluationMethod.AnyBalance = 1;
+EvaluationMethod.SpecificToken = 2;
+
+EvaluationMethod.Modes = [
+    EvaluationMethod.None,
+    EvaluationMethod.AnyBalance,
+    EvaluationMethod.SpecificToken
+];
+
+// Export
+if (NODE) {
+    module.exports = EvaluationMethod;
+} else {
+    // Namespace the export in browsers
+    if (window) {
+        if (!window.Boson) window.Boson = {};
+        window.Boson.EvaluationMethod = EvaluationMethod;
+    }
+}
