3951a2fa808be9ddbbe85954e841a7d6ebe2dcd1
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2022-03-14 22:53:40 +0530

    Twin domain related changes

---
+++ test/domain/TwinTest.js
@@ -0,0 +1,312 @@
+const { expect } = require("chai");
+const Twin = require("../../scripts/domain/Twin.js");
+
+/**
+ *  Test the Twin domain entity
+ */
+describe("Twin", function() {
+
+    // Suite-wide scope
+    let twin, object, promoted, clone, dehydrated, rehydrated, key, value;
+    let id, sellerId, supplyAvailable, supplyIds, tokenId, tokenAddress;
+
+    context("📋 Constructor", async function () {
+
+        beforeEach( async function () {
+            // Get a list of accounts
+            accounts = await ethers.getSigners();
+
+            // Required constructor params
+            id = sellerId = supplyAvailable = tokenId = "21";
+            supplyIds = ['1', '2']
+            tokenAddress = accounts[0].address;
+        });
+
+        it("Should allow creation of valid, fully populated Twin instance", async function () {
+
+            twin = new Twin(id, sellerId, supplyAvailable, supplyIds, tokenId, tokenAddress);
+            expect(twin.idIsValid()).is.true;
+            expect(twin.sellerIdIsValid()).is.true;
+            expect(twin.supplyAvailableIsValid()).is.true;
+            expect(twin.supplyIdsIsValid()).is.true;
+            expect(twin.tokenIdIsValid()).is.true;
+            expect(twin.tokenAddressIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+    });
+
+    context("📋 Field validations", async function () {
+
+        beforeEach( async function () {
+
+            // Required constructor params
+            id = sellerId = supplyAvailable = tokenId = "51";
+
+            // Create a valid twin, then set fields in tests directly
+            twin = new Twin(id, sellerId, supplyAvailable, supplyIds, tokenId, tokenAddress);
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, id must be the string representation of a BigNumber", async function() {
+
+            // Invalid field value
+            twin.id = "zedzdeadbaby";
+            expect(twin.idIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.id = new Date();
+            expect(twin.idIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.id = 12;
+            expect(twin.idIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.id = "0";
+            expect(twin.idIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.id = "126";
+            expect(twin.idIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, sellerId must be the string representation of a BigNumber", async function() {
+
+            // Invalid field value
+            twin.sellerId = "zedzdeadbaby";
+            expect(twin.sellerIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.sellerId = new Date();
+            expect(twin.sellerIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.sellerId = 12;
+            expect(twin.sellerIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.sellerId = "0";
+            expect(twin.sellerIdIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.sellerId = "126";
+            expect(twin.sellerIdIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, supplyAvailable must be the string representation of a BigNumber", async function() {
+
+            // Invalid field value
+            twin.supplyAvailable = "zedzdeadbaby";
+            expect(twin.supplyAvailableIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.supplyAvailable = new Date();
+            expect(twin.supplyAvailableIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.supplyAvailable = 12;
+            expect(twin.supplyAvailableIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.supplyAvailable = "0";
+            expect(twin.supplyAvailableIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.supplyAvailable = "126";
+            expect(twin.supplyAvailableIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, supplyIds must be the array containing string representation of a BigNumber", async function() {
+
+            // Invalid field value
+            twin.supplyIds = "zedzdeadbaby";
+            expect(twin.supplyIdsIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.supplyIds = new Date();
+            expect(twin.supplyIdsIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.supplyIds = 12;
+            expect(twin.supplyIdsIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.supplyIds = ["1", "2"];
+            expect(twin.supplyIdsIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.supplyIds = ["126"];
+            expect(twin.supplyIdsIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, tokenId must be the string representation of a BigNumber", async function() {
+
+            // Invalid field value
+            twin.tokenId = "zedzdeadbaby";
+            expect(twin.tokenIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.tokenId = new Date();
+            expect(twin.tokenIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.tokenId = 12;
+            expect(twin.tokenIdIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.tokenId = "0";
+            expect(twin.tokenIdIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.tokenId = "126";
+            expect(twin.tokenIdIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+
+        it("Always present, tokenAddress must be a string representation of an EIP-55 compliant address", async function() {
+
+            // Invalid field value
+            twin.tokenAddress = "0xASFADF";
+            expect(twin.tokenAddressIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Invalid field value
+            twin.tokenAddress = "zedzdeadbaby";
+            expect(twin.tokenAddressIsValid()).is.false;
+            expect(twin.isValid()).is.false;
+
+            // Valid field value
+            twin.tokenAddress = accounts[0].address;
+            expect(twin.tokenAddressIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+            // Valid field value
+            twin.tokenAddress = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
+            expect(twin.tokenAddressIsValid()).is.true;
+            expect(twin.isValid()).is.true;
+
+        });
+    })
+
+    context("📋 Utility functions", async function () {
+
+        beforeEach( async function () {
+
+            // Required constructor params
+            id = sellerId = supplyAvailable = tokenId = "90125";
+            supplyIds = ['123', '456']
+            tokenAddress = accounts[0].address;
+
+            // Create a valid twin, then set fields in tests directly
+            twin = new Twin(id, sellerId, supplyAvailable, supplyIds, tokenId, tokenAddress);
+            expect(twin.isValid()).is.true;
+
+            // Get plain object
+            object = {
+                id,
+                sellerId,
+                supplyAvailable,
+                supplyIds,
+                tokenId,
+                tokenAddress
+            }
+
+        })
+
+        context("👉 Static", async function () {
+
+            it("Twin.fromObject() should return a Twin instance with the same values as the given plain object", async function () {
+
+                // Promote to instance
+                promoted = Twin.fromObject(object);
+
+                // Is a Twin instance
+                expect(promoted instanceof Twin).is.true;
+
+                // Key values all match
+                for ([key, value] of Object.entries(twin)) {
+                    expect(JSON.stringify(promoted[key]) === JSON.stringify(value)).is.true;
+                }
+
+            });
+
+        });
+
+        context("👉 Instance", async function () {
+
+            it("instance.toString() should return a JSON string representation of the Twin instance", async function () {
+
+                dehydrated = twin.toString();
+                rehydrated = JSON.parse(dehydrated);
+
+                for ([key, value] of Object.entries(twin)) {
+                    expect(JSON.stringify(rehydrated[key]) === JSON.stringify(value)).is.true;
+                }
+
+            });
+
+            it("instance.clone() should return another Twin instance with the same property values", async function () {
+
+                // Get plain object
+                clone = twin.clone();
+
+                // Is an Twin instance
+                expect(clone instanceof Twin).is.true;
+
+                // Key values all match
+                for ([key, value] of Object.entries(twin)) {
+                    expect(JSON.stringify(clone[key]) === JSON.stringify(value)).is.true;
+                }
+
+            });
+
+            it("instance.toObject() should return a plain object representation of the Twin instance", async function () {
+
+                // Get plain object
+                object = twin.toObject();
+
+                // Not an Twin instance
+                expect(object instanceof Twin).is.false;
+
+                // Key values all match
+                for ([key, value] of Object.entries(twin)) {
+                    expect(JSON.stringify(object[key]) === JSON.stringify(value)).is.true;
+                }
+
+            });
+        });
+
+    })
+});
