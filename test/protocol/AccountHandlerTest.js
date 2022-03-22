4c3a0e63ac0f5d63b8ef0ddb694ace735390bce4
zajck <klemen@impressieve.com>
2022-03-22 09:39:26 +0100

    fix importing interface ids

--- test/protocol/AccountHandlerTest.js
+++ test/protocol/AccountHandlerTest.js
@@ -3,7 +3,7 @@ const ethers = hre.ethers;
 const { expect } = require("chai");
 
 const Role = require("../../scripts/domain/Role");
-const { InterfaceIds } = require('../../scripts/config/supported-interfaces.js');
+const { getInterfaceIds } = require('../../scripts/config/supported-interfaces.js');
 const { RevertReasons } = require('../../scripts/config/revert-reasons.js');
 const { deployProtocolDiamond } = require('../../scripts/util/deploy-protocol-diamond.js');
 const { deployProtocolHandlerFacets } = require('../../scripts/util/deploy-protocol-handler-facets.js');
@@ -14,9 +14,17 @@ const { deployProtocolHandlerFacets } = require('../../scripts/util/deploy-proto
  describe("IBosonAccountHandler", function() {
 
     // Common vars
+    let InterfaceIds;
     let accounts, deployer, rando;
     let erc165, protocolDiamond, diamondLoupe, diamondCut, accessController, accountHandler, accountrHandlerFacet, accountStruct;
     let expected, nextAccountId;
+
+    before (async function() {
+        
+        // get interface Ids    
+        InterfaceIds = await getInterfaceIds();
+    
+    })
 
     beforeEach( async function () {
 
