a3f7161f1b62584eee1f15c122ccf1ec9fa35465
zajck <klemen@impressieve.com>
2022-04-12 10:05:00 +0200

    happy path tests

--- contracts/interfaces/IBosonComboHandler.sol
+++ contracts/interfaces/IBosonComboHandler.sol
@@ -13,4 +13,20 @@ import "../domain/BosonTypes.sol";
 interface IBosonComboHandler {
     /// Events
     event OfferCreated(uint256 indexed offerId, uint256 indexed sellerId, BosonTypes.Offer offer);
+
+    /**
+     * @notice Creates a seller and an offer in a single transaction.
+     *
+     * Emits a SellerCreated and an OfferCreated event if successful.
+     *
+     * Reverts if:
+     * - seller does not exist
+     * - Valid from date is greater than valid until date
+     * - Valid until date is not in the future
+     * - Buyer cancel penalty is greater than price
+     * - Voided is set to true
+     *
+     * @param _offer - the fully populated struct with offer id set to 0x0 and voided set to false
+     */
+    function createSellerAndOffer(BosonTypes.Seller calldata _seller, BosonTypes.Offer memory _offer) external;
 }
---
+++ contracts/protocol/facets/ComboHandlerFacet.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import { IBosonComboHandler } from "../../interfaces/IBosonComboHandler.sol";
+import { IBosonAccountHandler } from "../../interfaces/IBosonAccountHandler.sol";
+import { DiamondLib } from "../../diamond/DiamondLib.sol";
+import { OfferBase } from "../OfferBase.sol";
+
+/**
+ * @title ComboHandlerFacet
+ *
+ * @notice Combines creation of multiple entities (accounts, offers, groups, twins, bundles) in a single transaction
+ */
+contract ComboHandlerFacet is OfferBase, IBosonComboHandler {
+
+    /**
+     * @notice Facet Initializer
+     */
+    function initialize()
+    public
+    onlyUnInitialized(type(IBosonComboHandler).interfaceId)
+    {
+        DiamondLib.addSupportedInterface(type(IBosonComboHandler).interfaceId);
+    }
+
+    /**
+     * @notice Creates a seller and an offer in a single transaction.
+     *
+     * Emits a SellerCreated and an OfferCreated event if successful.
+     *
+     * Reverts if:
+     * - seller does not exist
+     * - Valid from date is greater than valid until date
+     * - Valid until date is not in the future
+     * - Buyer cancel penalty is greater than price
+     * - Voided is set to true
+     *
+     * @param _offer - the fully populated struct with offer id set to 0x0 and voided set to false
+     */
+    function createSellerAndOffer(
+        Seller calldata _seller,
+        Offer memory _offer
+    )
+    external
+    
+    {   
+        IBosonAccountHandler(address(this)).createSeller(_seller);    
+        
+        // create offer and update structs values to represent true state
+        (uint256 offerId, uint256 sellerId) = createOfferInternal(_offer);
+        _offer.id = offerId;
+        _offer.sellerId = sellerId;
+
+        emit OfferCreated(offerId, sellerId, _offer);
+    }  
+}
