6c8984d36d4992241c32c0198192566478055d64
zajck <klemen@impressieve.com>
2022-04-13 16:47:14 +0200

    AccountBase

--- contracts/protocol/facets/AccountHandlerFacet.sol
+++ contracts/protocol/facets/AccountHandlerFacet.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 import { IBosonAccountHandler } from "../../interfaces/IBosonAccountHandler.sol";
 import { DiamondLib } from "../../diamond/DiamondLib.sol";
-import { ProtocolBase } from "../ProtocolBase.sol";
+import { AccountBase } from "../bases/AccountBase.sol";
 import { ProtocolLib } from "../ProtocolLib.sol";
 
-contract AccountHandlerFacet is IBosonAccountHandler, ProtocolBase {
+contract AccountHandlerFacet is IBosonAccountHandler, AccountBase {
 
     /**
      * @notice Facet Initializer
@@ -35,23 +35,12 @@ contract AccountHandlerFacet is IBosonAccountHandler, ProtocolBase {
     external
     override
     {
-        //Check active is not set to false
-        require(_seller.active, MUST_BE_ACTIVE);
-
-        // Get the next account Id and increment the counter
-        uint256 sellerId = protocolCounters().nextAccountId++;
-
-        //check that the addresses are unique to one seller Id
-        require(protocolStorage().sellerIdByOperator[_seller.operator] == 0 && 
-                protocolStorage().sellerIdByAdmin[_seller.admin] == 0 && 
-                protocolStorage().sellerIdByClerk[_seller.clerk] == 0,  
-                SELLER_ADDRESS_MUST_BE_UNIQUE);
-
+        // create seller and update structs values to represent true state
+        uint256 sellerId = createSellerInternal(_seller);
         _seller.id = sellerId;
-        storeSeller(_seller);
 
         // Notify watchers of state change
-        emit SellerCreated(_seller.id, _seller);
+        emit SellerCreated(sellerId, _seller);
     }
 
     /**
@@ -209,43 +198,6 @@ contract AccountHandlerFacet is IBosonAccountHandler, ProtocolBase {
     view 
     returns(uint256 nextAccountId) {
         nextAccountId = protocolCounters().nextAccountId;
-    }
-
-    /**
-     * @notice Validates seller struct and stores it to storage
-     *
-     * Reverts if:
-     * - Address values are zero address
-     * - Addresses are not unique to this seller
-     *
-     * @param _seller - the fully populated struct with seller id set
-     */
-   
-    function storeSeller(Seller memory _seller) internal 
-    {
-        //Check for zero address
-        require(_seller.admin != address(0) &&  
-                _seller.operator != address(0) && 
-                _seller.clerk != address(0) && 
-                _seller.treasury != address(0), 
-                INVALID_ADDRESS);
-
-        // Get storage location for seller
-        (,Seller storage seller) = fetchSeller(_seller.id);
-
-        // Set seller props individually since memory structs can't be copied to storage
-        seller.id = _seller.id;
-        seller.operator = _seller.operator;
-        seller.admin = _seller.admin;
-        seller.clerk = _seller.clerk;
-        seller.treasury = _seller.treasury;
-        seller.active = _seller.active;
-
-        //Map the seller's addresses to the seller Id. It's not necessary to map the treasury address, as it only receives funds
-        protocolStorage().sellerIdByOperator[_seller.operator] = _seller.id;
-        protocolStorage().sellerIdByAdmin[_seller.admin] = _seller.id;
-        protocolStorage().sellerIdByClerk[_seller.clerk] = _seller.id;
-       
     }
 
     /**
