d7aa76e12eaa1ceaa1793c119fa45e7460f795a5
zajck <64400885+zajck@users.noreply.github.com>
2022-04-21 16:34:47 +0200

    Orchestration - add seller creation to the existing methods (#67)
    * createOfferAndTwinWithBundle solidity code
    
    * createOfferAndTwinWithBundle happy path unit test
    
    * createOfferAndTwinWithBundle revert reason unit test
    
    * correct interface in natspec
    
    * added seller creation to two existing methods
    
    * unit tests with create seller
    
    * interface id update
    
    * indentation fix

--- contracts/interfaces/handlers/IBosonOrchestrationHandler.sol
+++ contracts/interfaces/handlers/IBosonOrchestrationHandler.sol
@@ -13,7 +13,7 @@ import {IBosonBundleEvents} from "../events/IBosonBundleEvents.sol";
  *
  * @notice Combines creation of multiple entities (accounts, offers, groups, twins, bundles) in a single transaction
  *
- * The ERC-165 identifier for this interface is: 0x8db6d85b
+ * The ERC-165 identifier for this interface is: 0xea8bc888
  */
 interface IBosonOrchestrationHandler is IBosonAccountEvents, IBosonGroupEvents, IBosonOfferEvents, IBosonTwinEvents, IBosonBundleEvents {
     /**
@@ -83,4 +83,66 @@ interface IBosonOrchestrationHandler is IBosonAccountEvents, IBosonGroupEvents, 
         BosonTypes.Twin memory _twin
     )
     external;
+
+    /**
+     * @notice Takes a seller, an offer and a condition, creates a seller, creates an offer, then a group with that offer and the given condition.
+     *
+     * Emits a SellerCreated, an OfferCreated and a GroupCreated event if successful.
+     *
+     * Reverts if:
+     * - caller is not the same as operator address
+     * - in seller struct:
+     *   - Address values are zero address
+     *   - Addresses are not unique to this seller
+     *   - Seller is not active (if active == false)
+     * - in offer struct:
+     *   - Caller is not an operator
+     *   - Valid from date is greater than valid until date
+     *   - Valid until date is not in the future
+     *   - Buyer cancel penalty is greater than price
+     *   - Voided is set to true
+     * - Condition includes invalid combination of parameters
+     *
+     * @param _seller - the fully populated seller struct
+     * @param _offer - the fully populated struct with offer id set to 0x0 and voided set to false
+     * @param _condition - the fully populated condition struct
+     */
+    function createSellerAndOfferWithCondition(
+        BosonTypes.Seller memory _seller,
+        BosonTypes.Offer memory _offer,
+        BosonTypes.Condition memory _condition
+    )
+    external;
+
+    /**
+     * @notice Takes a seller, an offer and a twin, creates a seller, creates an offer, creates a twin, then a bundle with that offer and the given twin
+     *
+     * Emits a SellerCreated, an OfferCreated, a TwinCreated and a BundleCreated event if successful.
+     *
+     * Reverts if:
+     * - caller is not the same as operator address
+     * - in seller struct:
+     *   - Address values are zero address
+     *   - Addresses are not unique to this seller
+     *   - Seller is not active (if active == false)
+     * - in offer struct:
+     *   - Caller is not an operator
+     *   - Valid from date is greater than valid until date
+     *   - Valid until date is not in the future
+     *   - Buyer cancel penalty is greater than price
+     *   - Voided is set to true
+     * - when creating twin if
+     *   - Not approved to transfer the seller's token
+     *
+     * @param _seller - the fully populated seller struct
+     * @param _offer - the fully populated struct with offer id set to 0x0 and voided set to false
+     * @param _twin - the fully populated twin struct
+     */
+    function createSellerAndOfferAndTwinWithBundle(
+        BosonTypes.Seller memory _seller,
+        BosonTypes.Offer memory _offer,
+        BosonTypes.Twin memory _twin
+    )
+    external;
+
 }
