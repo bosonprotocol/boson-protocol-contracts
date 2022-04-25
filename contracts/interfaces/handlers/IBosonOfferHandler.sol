2acdc6fbe4a6c8ddeefcc432561e5bb7c0b1e454
zajck <klemen@impressieve.com>
2022-04-25 10:56:06 +0200

    natspec interfaceid update

--- contracts/interfaces/handlers/IBosonOfferHandler.sol
+++ contracts/interfaces/handlers/IBosonOfferHandler.sol
@@ -9,7 +9,7 @@ import {IBosonOfferEvents} from "../events/IBosonOfferEvents.sol";
  *
  * @notice Handles creation, voiding, and querying of offers within the protocol.
  *
- * The ERC-165 identifier for this interface is: 0x65defc13
+ * The ERC-165 identifier for this interface is: 0xbea4053c
  */
 interface IBosonOfferHandler is IBosonOfferEvents {
 
