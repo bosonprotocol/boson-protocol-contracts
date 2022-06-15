356044492d62f124f117204528a6ae17ad2c669e
Ana Julia Bittencourt <anajuliabit@gmail.com>
2022-06-15 14:40:15 -0300

    Refactor createBuyer to AccountBase (#152)
    * Refactor createBuyer from ExchangeHandlerFacet and AccountHandlerFacet to AccountBase
    
    * Return buyerId from getValidBuyer
    
    * Add NatSpec comment to getValidBuyer function
    
    * Fix comment identation
    
    Co-authored-by: Cliff Hall <cliff@futurescale.com>

--- contracts/protocol/bases/AccountBase.sol
+++ contracts/protocol/bases/AccountBase.sol
@@ -45,6 +45,59 @@ contract AccountBase is ProtocolBase, IBosonAccountEvents {
         emit SellerCreated(sellerId, _seller);
     }
 
+       /**
+     * @notice Creates a Buyer
+     *
+     * Emits an BuyerCreated event if successful.
+     *
+     * Reverts if:
+     * - Wallet address is zero address
+     * - Active is not true
+     * - Wallet address is not unique to this buyer
+     *
+     * @param _buyer - the fully populated struct with buyer id set to 0x0
+     */
+    function createBuyerInternal(Buyer memory _buyer) 
+    internal
+    {
+        //Check for zero address
+        require(_buyer.wallet != address(0), INVALID_ADDRESS);
+
+        //Check active is not set to false
+        require(_buyer.active, MUST_BE_ACTIVE);
+
+        // Get the next account Id and increment the counter
+        uint256 buyerId = protocolCounters().nextAccountId++;
+
+        //check that the wallet address is unique to one buyer Id
+        require(protocolLookups().buyerIdByWallet[_buyer.wallet] == 0, BUYER_ADDRESS_MUST_BE_UNIQUE);
+
+        _buyer.id = buyerId;
+        storeBuyer(_buyer);
+
+        //Notify watchers of state change
+        emit BuyerCreated(_buyer.id, _buyer);
+    }
+
+    /**
+     * @notice Stores buyer struct in storage
+     *
+     * @param _buyer - the fully populated struct with buyer id set
+     */
+    function storeBuyer(Buyer memory _buyer) internal 
+    {
+        // Get storage location for buyer
+        (,Buyer storage buyer) = fetchBuyer(_buyer.id);
+
+        // Set buyer props individually since memory structs can't be copied to storage
+        buyer.id = _buyer.id;
+        buyer.wallet = _buyer.wallet;
+        buyer.active = _buyer.active;
+
+        //Map the buyer's wallet address to the buyerId.
+        protocolLookups().buyerIdByWallet[_buyer.wallet] = _buyer.id;
+    }
+
     /**
      * @notice Validates seller struct and stores it to storage
      *
