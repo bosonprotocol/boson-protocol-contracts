5843c2b8fa49c0345047dd916410324676ab5e4e
bidyut-arianelabs <91169722+bidyut-arianelabs@users.noreply.github.com>
2022-06-16 02:19:38 +0530

    Meta transactions for raise dispute (#149)
    * solidity side of signature verification
    
    * domain model MetaTxDisputeDetails
    
    * unit tests: executeMetaTxRaiseDispute
    
    Co-authored-by: zajck <64400885+zajck@users.noreply.github.com>

--- contracts/interfaces/handlers/IBosonMetaTransactionsHandler.sol
+++ contracts/interfaces/handlers/IBosonMetaTransactionsHandler.sol
@@ -9,7 +9,7 @@ import {IBosonMetaTransactionsEvents} from "../events/IBosonMetaTransactionsEven
  *
  * @notice Manages incoming meta-transactions in the protocol.
  *
- * The ERC-165 identifier for this interface is: 0xc387cb03
+ * The ERC-165 identifier for this interface is: 0x3be9ecfa
  */
 interface IBosonMetaTransactionsHandler is IBosonMetaTransactionsEvents {
 
@@ -187,6 +187,30 @@ interface IBosonMetaTransactionsHandler is IBosonMetaTransactionsEvents {
     function executeMetaTxRetractDispute(
         address _userAddress,
         BosonTypes.MetaTxExchangeDetails calldata _exchangeDetails,
+        uint256 _nonce,
+        bytes32 _sigR,
+        bytes32 _sigS,
+        uint8 _sigV
+    ) external returns (bytes memory);
+
+    /**
+     * @notice Handles the incoming meta transaction for Raise Dispute.
+     *
+     * Reverts if:
+     * - nonce is already used by another transaction.
+     * - sender does not match the recovered signer.
+     * - any code executed in the signed transaction reverts.
+     *
+     * @param _userAddress - the sender of the transaction.
+     * @param _disputeDetails - the fully populated BosonTypes.MetaTxDisputeDetails struct.
+     * @param _nonce - the nonce value of the transaction.
+     * @param _sigR - r part of the signer's signature.
+     * @param _sigS - s part of the signer's signature.
+     * @param _sigV - v part of the signer's signature.
+     */
+    function executeMetaTxRaiseDispute(
+        address _userAddress,
+        BosonTypes.MetaTxDisputeDetails calldata _disputeDetails,
         uint256 _nonce,
         bytes32 _sigR,
         bytes32 _sigS,
