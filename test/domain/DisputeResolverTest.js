1add619da6f2b3b785989c706f8c197480309291
Heaher S <heatherswope@bosonprotocol.io>
2022-06-20 10:52:42 +0200

    Modify DisputeResolver domain script and unit test

--- test/domain/DisputeResolverTest.js
+++ test/domain/DisputeResolverTest.js
@@ -9,24 +9,38 @@ const DisputeResolver = require("../../scripts/domain/DisputeResolver");
 describe("DisputeResolver", function () {
   // Suite-wide scope
   let disputeResolver, object, promoted, clone, dehydrated, rehydrated, key, value, struct;
-  let accounts, id, wallet, active;
+  let accounts, id, escalationResponsePeriod, operator, admin, clerk, treasury, metadataUri, active;
+  let oneWeek;
 
   beforeEach(async function () {
     // Get a list of accounts
     accounts = await ethers.getSigners();
-    wallet = accounts[1].address;
+    operator = accounts[0].address;
+    admin = accounts[1].address;
+    clerk = accounts[2].address;
+    treasury = accounts[3].address;
+
+     // A period in milliseconds
+     oneWeek = 604800 * 1000; //  7 days in milliseconds
 
     // Required constructor params
     id = "170";
     active = true;
+    escalationResponsePeriod = oneWeek.toString();
+    metadataUri = `https://ipfs.io/ipfs/disputeResolver1`;
   });
 
   context("📋 Constructor", async function () {
     it("Should allow creation of valid, fully populated DisputeResolver instance", async function () {
       // Create a valid dispute resolver
-      disputeResolver = new DisputeResolver(id, wallet, active);
+      disputeResolver = new DisputeResolver(id, escalationResponsePeriod, operator, admin, clerk, treasury, metadataUri, active);
       expect(disputeResolver.idIsValid()).is.true;
-      expect(disputeResolver.walletIsValid()).is.true;
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.true;
+      expect(disputeResolver.operatorIsValid()).is.true;
+      expect(disputeResolver.adminIsValid()).is.true;
+      expect(disputeResolver.clerkIsValid()).is.true;
+      expect(disputeResolver.treasuryIsValid()).is.true;
+      expect(disputeResolver.metadataUriIsValid()).is.true;
       expect(disputeResolver.activeIsValid()).is.true;
       expect(disputeResolver.isValid()).is.true;
     });
@@ -35,7 +49,7 @@ describe("DisputeResolver", function () {
   context("📋 Field validations", async function () {
     beforeEach(async function () {
       // Create a valid dispute resolver, then set fields in tests directly
-      disputeResolver = new DisputeResolver(id, wallet, active);
+      disputeResolver = new DisputeResolver(id, escalationResponsePeriod, operator, admin, clerk, treasury, metadataUri, active);
       expect(disputeResolver.isValid()).is.true;
     });
 
@@ -66,25 +80,135 @@ describe("DisputeResolver", function () {
       expect(disputeResolver.isValid()).is.true;
     });
 
-    it("Always present, wallet must be a string representation of an EIP-55 compliant address", async function () {
+    it("Always present, escalationResponsePeriod must be the string representation of a BigNumber", async function () {
       // Invalid field value
-      disputeResolver.wallet = "0xASFADF";
-      expect(disputeResolver.walletIsValid()).is.false;
+      disputeResolver.escalationResponsePeriod = "zedzdeadbaby";
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.false;
       expect(disputeResolver.isValid()).is.false;
 
       // Invalid field value
-      disputeResolver.wallet = "zedzdeadbaby";
-      expect(disputeResolver.walletIsValid()).is.false;
+      disputeResolver.escalationResponsePeriod = new Date();
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Invalid field value
+      disputeResolver.escalationResponsePeriod = 12;
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.false;
       expect(disputeResolver.isValid()).is.false;
 
       // Valid field value
-      disputeResolver.wallet = accounts[0].address;
-      expect(disputeResolver.walletIsValid()).is.true;
+      disputeResolver.escalationResponsePeriod = "0";
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.true;
       expect(disputeResolver.isValid()).is.true;
 
       // Valid field value
-      disputeResolver.wallet = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
-      expect(disputeResolver.walletIsValid()).is.true;
+      disputeResolver.escalationResponsePeriod = "126";
+      expect(disputeResolver.escalationResponsePeriodIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+    });
+
+    it("Always present, operator must be a string representation of an EIP-55 compliant address", async function () {
+      // Invalid field value
+      disputeResolver.operator = "0xASFADF";
+      expect(disputeResolver.operatorIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Invalid field value
+      disputeResolver.operator = "zedzdeadbaby";
+      expect(disputeResolver.operatorIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Valid field value
+      disputeResolver.operator = accounts[0].address;
+      expect(disputeResolver.operatorIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+
+      // Valid field value
+      disputeResolver.operator = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
+      expect(disputeResolver.operatorIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+    });
+
+    it("Always present, admin must be a string representation of an EIP-55 compliant address", async function () {
+      // Invalid field value
+      disputeResolver.admin = "0xASFADF";
+      expect(disputeResolver.adminIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Invalid field value
+      disputeResolver.admin = "zedzdeadbaby";
+      expect(disputeResolver.adminIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Valid field value
+      disputeResolver.admin = accounts[0].address;
+      expect(disputeResolver.adminIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+
+      // Valid field value
+      disputeResolver.admin = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
+      expect(disputeResolver.adminIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+    });
+
+    it("Always present, clerk must be a string representation of an EIP-55 compliant address", async function () {
+      // Invalid field value
+      disputeResolver.clerk = "0xASFADF";
+      expect(disputeResolver.clerkIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Invalid field value
+      disputeResolver.clerk = "zedzdeadbaby";
+      expect(disputeResolver.clerkIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Valid field value
+      disputeResolver.clerk = accounts[0].address;
+      expect(disputeResolver.clerkIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+
+      // Valid field value
+      disputeResolver.clerk = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
+      expect(disputeResolver.clerkIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+    });
+
+    it("Always present, treasury must be a string representation of an EIP-55 compliant address", async function () {
+      // Invalid field value
+      disputeResolver.treasury = "0xASFADF";
+      expect(disputeResolver.treasuryIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Invalid field value
+      disputeResolver.treasury = "zedzdeadbaby";
+      expect(disputeResolver.treasuryIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Valid field value
+      disputeResolver.treasury = accounts[0].address;
+      expect(disputeResolver.treasuryIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+
+      // Valid field value
+      disputeResolver.treasury = "0xec2fd5bd6fc7b576dae82c0b9640969d8de501a2";
+      expect(disputeResolver.treasuryIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+    });
+
+    it("Always present, metadataUri must be a non-empty string", async function () {
+      // Invalid field value
+      disputeResolver.metadataUri = 12;
+      expect(disputeResolver.metadataUriIsValid()).is.false;
+      expect(disputeResolver.isValid()).is.false;
+
+      // Valid field value
+      disputeResolver.metadataUri = "zedzdeadbaby";
+      expect(disputeResolver.metadataUriIsValid()).is.true;
+      expect(disputeResolver.isValid()).is.true;
+
+      // Valid field value
+      disputeResolver.metadataUri = "https://ipfs.io/ipfs/QmYXc12ov6F2MZVZwPs5XeCBbf61cW3wKRk8h3D5NTYj4T";
+      expect(disputeResolver.metadataUriIsValid()).is.true;
       expect(disputeResolver.isValid()).is.true;
     });
 
@@ -114,18 +238,23 @@ describe("DisputeResolver", function () {
   context("📋 Utility functions", async function () {
     beforeEach(async function () {
       // Create a valid dispute resolver, then set fields in tests directly
-      disputeResolver = new DisputeResolver(id, wallet, active);
+      disputeResolver = new DisputeResolver(id, escalationResponsePeriod, operator, admin, clerk, treasury, metadataUri, active);
       expect(disputeResolver.isValid()).is.true;
 
       // Get plain object
       object = {
         id,
-        wallet,
+        escalationResponsePeriod,
+        operator, 
+        admin, 
+        clerk, 
+        treasury, 
+        metadataUri, 
         active,
       };
 
       // Struct representation
-      struct = [id, wallet, active];
+      struct = [id, escalationResponsePeriod, operator, admin, clerk, treasury, metadataUri, active];
     });
 
     context("👉 Static", async function () {
