78336554635e669ab0e8b807c82ded39e9222673
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2022-07-20 16:09:06 +0530

    unit tests: Update an agent

--- contracts/interfaces/handlers/IBosonAccountHandler.sol
+++ contracts/interfaces/handlers/IBosonAccountHandler.sol
@@ -9,7 +9,7 @@ import { IBosonAccountEvents } from "../events/IBosonAccountEvents.sol";
  *
  * @notice Handles creation, update, retrieval of accounts within the protocol.
  *
- * The ERC-165 identifier for this interface is: 0xd59469eb
+ * The ERC-165 identifier for this interface is: 0x3ef28879
  */
 interface IBosonAccountHandler is IBosonAccountEvents {
     /**
