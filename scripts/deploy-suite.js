ed0fd8b685335f558014c0088cf0577abe6d5fcd
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2022-08-02 20:04:10 +0530

    review fix

--- scripts/deploy-suite.js
+++ scripts/deploy-suite.js
@@ -120,7 +120,7 @@ function getAuthTokenContracts() {
     mainnet: "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d",
     hardhat: "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82",
     test: ethers.constants.AddressZero,
-    mumbai: ethers.constants.AddressZero,
+    mumbai: "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82",
   };
 
   // ENS contract address
