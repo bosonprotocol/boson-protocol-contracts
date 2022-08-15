05129432379b2450ba7039a0760ea7ea6ada4cca
Heather <42716568+hswopeams@users.noreply.github.com>
2022-08-15 07:46:31 -0400

    Deploy  mock NFT Auth tokens to test env (#251)
    Co-authored-by: Ana Julia Bittencourt <anajuliabit@gmail.com>

---
+++ contracts/mock/MockNFTAuth721.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+/**
+ * @title MockNFTAuth721
+ *
+ * @notice Mock ERC-(721) NFT that represents NFT Auth tokens
+ */
+contract MockNFTAuth721 is ERC721Upgradeable {
+    string public constant TOKEN_NAME = "MockNFTAuth721";
+    string public constant TOKEN_SYMBOL = "721NFTAuth";
+
+    /**
+     * Mint a Sample NFT to a specified address
+     * @param _to - address to which token with specified id is minted
+     * @param _tokenId - the first token ID to mint
+     */
+    function mint(address _to, uint256 _tokenId) external {
+        _mint(_to, _tokenId);
+    }
+}
