cbce7093aa4e1040dd47be5144de96e744f2bd14
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2022-08-17 16:33:07 +0530

    unit tests: tests for pauser, client and fee collector

--- test/access/AccessControllerTest.js
+++ test/access/AccessControllerTest.js
@@ -8,12 +8,12 @@ const Role = require("../../scripts/domain/Role");
  */
 describe("AccessController", function () {
   // Shared args
-  let deployer, admin, protocol, upgrader, associate;
+  let deployer, admin, protocol, upgrader, associate, pauser, client, feeCollector;
   let AccessController, accessController, roleAdmin;
 
   beforeEach(async function () {
     // Make accounts available
-    [deployer, admin, protocol, upgrader, associate] = await ethers.getSigners();
+    [deployer, admin, protocol, upgrader, associate, pauser, client, feeCollector] = await ethers.getSigners();
 
     // Deploy the contract
     AccessController = await ethers.getContractFactory("AccessController");
@@ -35,6 +35,32 @@ describe("AccessController", function () {
     it("Deployer should not have UPGRADER role", async function () {
       // Check role
       expect(await accessController.hasRole(Role.UPGRADER, deployer.address), "Deployer has UPGRADER role").is.false;
+    });
+
+    it("Deployer should not have PAUSER role", async function () {
+      // Check role
+      expect(await accessController.hasRole(Role.PAUSER, deployer.address), "Deployer has PAUSER role").is.false;
+    });
+
+    it("Deployer should not have CLIENT role", async function () {
+      // Check role
+      expect(await accessController.hasRole(Role.CLIENT, deployer.address), "Deployer has CLIENT role").is.false;
+    });
+
+    it("Deployer should not have FEE_COLLECTOR role", async function () {
+      // Check role
+      expect(
+        await accessController.hasRole(Role.FEE_COLLECTOR, deployer.address),
+        "Deployer has FEE_COLLECTOR role"
+      ).is.false;
+    });
+
+    it("Deployer should not have any un managed value as role", async function () {
+      // Random unknown role
+      let role = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("random"));
+
+      // Check role
+      expect(await accessController.hasRole(role, deployer.address), "Deployer has a random role").is.false;
     });
   });
 
@@ -61,6 +87,30 @@ describe("AccessController", function () {
 
       // Test
       expect(roleAdmin === Role.ADMIN, "ADMIN role isn't UPGRADER role admin").is.true;
+    });
+
+    it("ADMIN role should be PAUSER role admin", async function () {
+      // Get PAUSER role admin
+      roleAdmin = await accessController.getRoleAdmin(Role.PAUSER);
+
+      // Test
+      expect(roleAdmin === Role.ADMIN, "ADMIN role isn't PAUSER role admin").is.true;
+    });
+
+    it("ADMIN role should be CLIENT role admin", async function () {
+      // Get CLIENT role admin
+      roleAdmin = await accessController.getRoleAdmin(Role.CLIENT);
+
+      // Test
+      expect(roleAdmin === Role.ADMIN, "ADMIN role isn't CLIENT role admin").is.true;
+    });
+
+    it("ADMIN role should be FEE_COLLECTOR role admin", async function () {
+      // Get FEE_COLLECTOR role admin
+      roleAdmin = await accessController.getRoleAdmin(Role.FEE_COLLECTOR);
+
+      // Test
+      expect(roleAdmin === Role.ADMIN, "ADMIN role isn't FEE_COLLECTOR role admin").is.true;
     });
   });
 
@@ -109,6 +159,39 @@ describe("AccessController", function () {
         "ADMIN role can't grant UPGRADER role"
       ).is.true;
     });
+
+    it("ADMIN role should be able to grant PAUSER role", async function () {
+      // Grant Role
+      try {
+        await accessController.connect(admin).grantRole(Role.PAUSER, pauser.address);
+      } catch (e) {}
+
+      // Test
+      expect(await accessController.hasRole(Role.PAUSER, pauser.address), "ADMIN role can't grant PAUSER role").is.true;
+    });
+
+    it("ADMIN role should be able to grant CLIENT role", async function () {
+      // Grant Role
+      try {
+        await accessController.connect(admin).grantRole(Role.CLIENT, client.address);
+      } catch (e) {}
+
+      // Test
+      expect(await accessController.hasRole(Role.CLIENT, client.address), "ADMIN role can't grant CLIENT role").is.true;
+    });
+
+    it("ADMIN role should be able to grant FEE_COLLECTOR role", async function () {
+      // Grant Role
+      try {
+        await accessController.connect(admin).grantRole(Role.FEE_COLLECTOR, feeCollector.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.FEE_COLLECTOR, feeCollector.address),
+        "ADMIN role can't grant FEE_COLLECTOR role"
+      ).is.true;
+    });
   });
 
   context("📋 Any ADMIN can revoke all other roles", async function () {
@@ -117,6 +200,9 @@ describe("AccessController", function () {
       await accessController.connect(deployer).grantRole(Role.ADMIN, admin.address);
       await accessController.connect(deployer).grantRole(Role.PROTOCOL, protocol.address);
       await accessController.connect(deployer).grantRole(Role.UPGRADER, upgrader.address);
+      await accessController.connect(deployer).grantRole(Role.PAUSER, pauser.address);
+      await accessController.connect(deployer).grantRole(Role.CLIENT, client.address);
+      await accessController.connect(deployer).grantRole(Role.FEE_COLLECTOR, feeCollector.address);
     });
 
     it("ADMIN role should be able to revoke ADMIN role", async function () {
@@ -157,6 +243,45 @@ describe("AccessController", function () {
         "ADMIN role can't revoke UPGRADER role"
       ).is.false;
     });
+
+    it("ADMIN role should be able to revoke PAUSER role", async function () {
+      // Revoke Role
+      try {
+        await accessController.connect(admin).revokeRole(Role.PAUSER, pauser.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.PAUSER, pauser.address),
+        "ADMIN role can't revoke PAUSER role"
+      ).is.false;
+    });
+
+    it("ADMIN role should be able to revoke CLIENT role", async function () {
+      // Revoke Role
+      try {
+        await accessController.connect(admin).revokeRole(Role.CLIENT, client.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.CLIENT, client.address),
+        "ADMIN role can't revoke CLIENT role"
+      ).is.false;
+    });
+
+    it("ADMIN role should be able to revoke FEE_COLLECTOR role", async function () {
+      // Revoke Role
+      try {
+        await accessController.connect(admin).revokeRole(Role.FEE_COLLECTOR, feeCollector.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.FEE_COLLECTOR, feeCollector.address),
+        "ADMIN role can't revoke FEE_COLLECTOR role"
+      ).is.false;
+    });
   });
 
   context("📋 Any roled address can renounce its roles", async function () {
@@ -165,6 +290,9 @@ describe("AccessController", function () {
       await accessController.connect(deployer).grantRole(Role.ADMIN, admin.address);
       await accessController.connect(deployer).grantRole(Role.PROTOCOL, protocol.address);
       await accessController.connect(deployer).grantRole(Role.UPGRADER, upgrader.address);
+      await accessController.connect(deployer).grantRole(Role.PAUSER, pauser.address);
+      await accessController.connect(deployer).grantRole(Role.CLIENT, client.address);
+      await accessController.connect(deployer).grantRole(Role.FEE_COLLECTOR, feeCollector.address);
     });
 
     it("ADMIN role should be able to renounce ADMIN role", async function () {
@@ -205,6 +333,45 @@ describe("AccessController", function () {
         "UPGRADER role can't renounce UPGRADER role"
       ).is.false;
     });
+
+    it("PAUSER role should be able to renounce PAUSER role", async function () {
+      // Renounce Role
+      try {
+        await accessController.connect(pauser).renounceRole(Role.PAUSER, pauser.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.PAUSER, pauser.address),
+        "PAUSER role can't renounce PAUSER role"
+      ).is.false;
+    });
+
+    it("CLIENT role should be able to renounce CLIENT role", async function () {
+      // Renounce Role
+      try {
+        await accessController.connect(client).renounceRole(Role.CLIENT, client.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.CLIENT, client.address),
+        "CLIENT role can't renounce CLIENT role"
+      ).is.false;
+    });
+
+    it("FEE_COLLECTOR role should be able to renounce FEE_COLLECTOR role", async function () {
+      // Renounce Role
+      try {
+        await accessController.connect(feeCollector).renounceRole(Role.FEE_COLLECTOR, feeCollector.address);
+      } catch (e) {}
+
+      // Test
+      expect(
+        await accessController.hasRole(Role.FEE_COLLECTOR, feeCollector.address),
+        "FEE_COLLECTOR role can't renounce FEE_COLLECTOR role"
+      ).is.false;
+    });
   });
 
   context("📋 Any address can have multiple roles", async function () {
@@ -219,12 +386,18 @@ describe("AccessController", function () {
         await accessController.connect(admin).grantRole(Role.ADMIN, associate.address);
         await accessController.connect(admin).grantRole(Role.PROTOCOL, associate.address);
         await accessController.connect(admin).grantRole(Role.UPGRADER, associate.address);
+        await accessController.connect(admin).grantRole(Role.PAUSER, associate.address);
+        await accessController.connect(admin).grantRole(Role.CLIENT, associate.address);
+        await accessController.connect(admin).grantRole(Role.FEE_COLLECTOR, associate.address);
       } catch (e) {}
 
       // Check roles all apply for associate
       expect(await accessController.hasRole(Role.ADMIN, associate.address)).is.true;
       expect(await accessController.hasRole(Role.PROTOCOL, associate.address)).is.true;
       expect(await accessController.hasRole(Role.UPGRADER, associate.address)).is.true;
+      expect(await accessController.hasRole(Role.PAUSER, associate.address)).is.true;
+      expect(await accessController.hasRole(Role.CLIENT, associate.address)).is.true;
+      expect(await accessController.hasRole(Role.FEE_COLLECTOR, associate.address)).is.true;
     });
   });
 });
