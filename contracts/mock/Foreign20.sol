d333b46aa96b39c3ccbc0129d6b5421409618230
zajck <klemen@impressieve.com>
2022-08-17 09:20:04 +0200

    removed console.sol import

--- contracts/mock/Foreign20.sol
+++ contracts/mock/Foreign20.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 /**
  * @title Foreign20
