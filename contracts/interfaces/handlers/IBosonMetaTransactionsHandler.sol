9d55a025f7e230782f5a5f90f06c225d89f90cd3
Heaher S <heatherswope@bosonprotocol.io>
2022-09-21 15:02:33 +0200

    Fix interface Id in NatSpec

--- contracts/interfaces/handlers/IBosonMetaTransactionsHandler.sol
+++ contracts/interfaces/handlers/IBosonMetaTransactionsHandler.sol
@@ -9,7 +9,7 @@ import { IBosonMetaTransactionsEvents } from "../events/IBosonMetaTransactionsEv
  *
  * @notice Manages incoming meta-transactions in the protocol.
  *
- * The ERC-165 identifier for this interface is: 0x369a01ef
+ * The ERC-165 identifier for this interface is: 0xd25fcdc1
  */
 interface IBosonMetaTransactionsHandler is IBosonMetaTransactionsEvents {
     /**
