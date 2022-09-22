fce5dcf48c24ca7280c79eb40d59ef4da45ccb63
Ana Julia <anajuliabit@gmail.com>
2022-09-22 11:33:31 -0300

    Fix interfaces id

--- contracts/interfaces/handlers/IBosonAccountHandler.sol
+++ contracts/interfaces/handlers/IBosonAccountHandler.sol
@@ -9,7 +9,7 @@ import { IBosonAccountEvents } from "../events/IBosonAccountEvents.sol";
  *
  * @notice Handles creation, update, retrieval of accounts within the protocol.
  *
- * The ERC-165 identifier for this interface is: 0xde21f0d4
+ * The ERC-165 identifier for this interface is: 0x6db2c812
  */
 interface IBosonAccountHandler is IBosonAccountEvents {
     /**
