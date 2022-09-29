2c4f4b53b5cd5afe4169c6e3d84ee6f42a268421
Ana Julia <anajuliabit@gmail.com>
2022-09-29 14:51:53 -0300

    Add comments explain the new scriptsg

--- scripts/util/split-tests-into-chunks.js
+++ scripts/util/split-tests-into-chunks.js
@@ -1,13 +1,19 @@
+// Script to split tests into chunks. Used on `test/utils/generate-test-chunks.sh`
 const fs = require("fs");
 
+// The number of chunks to split the tests into
 const chunks = process.argv[2];
-const filePath = process.argv[3];
-const files = JSON.parse(process.argv[4]);
+// The path to the temporary file with the outputs of `time` command, created on `test/utils/generate-test-chunks.sh` (`time-report.txt`)
+const timeReportFile = process.argv[3];
+// A list of tests to split
+let files = JSON.parse(process.argv[4]);
 
-fs.readFile(filePath, "utf8", (_, data) => {
+fs.readFile(timeReportFile, "utf8", (_, data) => {
+  // Each line contains the result of `time` command for a test following the same order of `files` list
   const lines = data.split("\n");
 
-  let result = files.map((file, index) => {
+  // Map the list of test names to a list of objects containing the test name and the time it took to run
+  files = files.map((file, index) => {
     const line = lines[index];
 
     if (line) {
@@ -16,30 +22,38 @@ fs.readFile(filePath, "utf8", (_, data) => {
     }
   });
 
-  result = result.filter((result) => !!result).sort((a, b) => a.time - b.time);
+  // Remove undefined values from the list if any and sort the list by the time it took to run
+  files = files.filter((result) => !!result).sort((a, b) => a.time - b.time);
 
-  const timeTotal = result.reduce((acc, result) => {
+  // Sum the total time of all tests
+  const timeTotal = files.reduce((acc, result) => {
     return acc + parseFloat(result.time);
   }, 0);
 
-  console.log(`Total time: ${timeTotal}s`);
+  console.log(`Total tests execution time: ${timeTotal}s`);
 
+  // Calculate the average time that each chunk should take
   const timePerChunk = timeTotal / chunks;
 
+  // Create a list of chunks
   const filesByChunk = [...Array(Number(chunks))].map(() => []);
 
   let currentChunk = 0;
   let currentChunkTime = 0;
 
-  for (const item of result) {
+  // Iterate over the list of files and add them to the chunks
+  for (const item of files) {
+    // If the sum of the current chunk time + the current file time is greater than the average time per chunk, move to the next chunk and reset currentChunkTime
     const sum = currentChunkTime + item.time;
     if (sum > timePerChunk && currentChunk < chunks - 1) {
       currentChunk++;
 
       currentChunkTime = item.time;
     } else {
+      // Otherwise add the current file time to the current chunk time
       currentChunkTime += item.time;
     }
+    // Add the current file name to the current chunk
     filesByChunk[currentChunk].push(item.name);
   }
 
--- test/utils/generate-test-chunks.sh
+++ test/utils/generate-test-chunks.sh
@@ -1,18 +1,23 @@
 
 #!/bin/sh
-# This script run the unit tests for the project and generat chunks of tests with approximatly the same execution time in order to run them in parallel on CI
+# This script runs the unit tests for the project and generates chunks of tests with approximatly the same execution time in order to run them in parallel on CI
+# We should run this script when we want to recalculate the chunks
 TEST_FILES=$(node ./scripts/util/find-test-files.js) 
 LENGTH=$(echo $TEST_FILES | jq -cM 'length')
 TIME_REPORT_PATH=./time-report.txt
 
+# Loop through all test files
 for (( i=0; i<$LENGTH; i++ )); do
   TEST_FILE=$(echo $TEST_FILES | jq -cM ".[$i]" | tr -d '"') 
-    
+
+  # Execute each test file individually and append the `time` command output on TIME_REPORT_PATH
   { time npx hardhat test $TEST_FILE ; } 2>> $TIME_REPORT_PATH
 
 done
 
+# Runs the script that generates the chunks of tests
 node scripts/util/split-tests-into-chunks.js 4 $TIME_REPORT_PATH $TEST_FILES
 
+# Remove the time report file since it's no longer needed
 rm $TIME_REPORT_PATH
 
