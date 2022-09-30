9807524e061ded50b1968f29ea93a380ba1456d1
Ana Julia <anajuliabit@gmail.com>
2022-09-30 16:59:28 -0300

    Change tidy scripts

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,12 @@
 module.exports = {
-    skipFiles: ['mock', 'protocol/bases/ClientBase.sol',  'protocol/clients/proxy/ClientProxy.sol', 'protocol/libs/ClientLib.sol'],
-    mocha: {
-      grep: "@skip-on-coverage", // Find everything with this tag
-      invert: true               // Run the grep's inverse set.
-    }
+  skipFiles: [
+    "mock",
+    "protocol/bases/ClientBase.sol",
+    "protocol/clients/proxy/ClientProxy.sol",
+    "protocol/libs/ClientLib.sol",
+  ],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
 };
