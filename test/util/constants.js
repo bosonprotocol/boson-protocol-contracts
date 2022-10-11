87a3ddfce146185f7a6af929ae03aabe5bbe34e1
Mischa <mischa@mmt.me.uk>
2022-10-11 16:58:03 +0100

    This makes the util constants file more legible when it comes to dates (#434)
    * This makes the util constants file more legible when it comes to dates
    and times
    
    * This cleans up the test file

--- test/util/constants.js
+++ test/util/constants.js
@@ -1,8 +1,8 @@
-// Some periods in milliseconds
+// Some periods in seconds
 const oneDay = 86400; //  1 day in seconds
 const ninetyDays = oneDay * 90; // 90 days in seconds
-const oneWeek = 604800; //  7 days in seconds
-const oneMonth = 2678400; // 31 days in seconds
+const oneWeek = oneDay * 7; // 7 days in seconds
+const oneMonth = oneDay * 31; // 31 days in seconds
 const VOUCHER_NAME = "Boson Voucher";
 const VOUCHER_SYMBOL = "BOSON_VOUCHER";
 
