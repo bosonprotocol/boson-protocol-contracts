efc0e9e65a51c0ad7bb5a2429737fbb0f56a3356
zajck <klemen@impressieve.com>
2022-10-21 13:56:41 +0200

    refactor to use same code for args and no-args

--- scripts/util/deploy-protocol-handler-facets.js
+++ scripts/util/deploy-protocol-handler-facets.js
@@ -17,47 +17,14 @@ const { getFees } = require("./utils");
  * @returns {Promise<(*|*|*)[]>}
  */
 async function deployProtocolHandlerFacets(diamond, facetNames, maxPriorityFeePerGas, doCut = true) {
-  let deployedFacets = [];
-
-  // Deploy all the no-arg initializer handler facets
-  while (facetNames.length) {
-    let facetName = facetNames.shift();
-    let FacetContractFactory = await ethers.getContractFactory(facetName);
-    const facetContract = await FacetContractFactory.deploy(await getFees(maxPriorityFeePerGas));
-    await facetContract.deployTransaction.wait(confirmations);
-
-    deployedFacets.push({
-      name: facetName,
-      contract: facetContract,
-    });
+  // Convert facetNames into expected facetData format
+  let facetData = {};
+  for (const facetName of facetNames) {
+    facetData[facetName] = "";
   }
 
-  if (doCut) {
-    // Cast Diamond to DiamondCutFacet
-    const diamondCutFacet = await ethers.getContractAt("DiamondCutFacet", diamond.address);
-
-    // All handler facets currently have no-arg initializers
-    let initFunction = "initialize()";
-    let initInterface = new ethers.utils.Interface([`function ${initFunction}`]);
-    let callData = initInterface.encodeFunctionData("initialize");
-
-    // Cut all the facets into the diamond
-    for (let i = 0; i < deployedFacets.length; i++) {
-      const deployedFacet = deployedFacets[i];
-
-      const facetCut = getFacetAddCut(deployedFacet.contract, [initFunction]);
-      const transactionResponse = await diamondCutFacet.diamondCut(
-        [facetCut],
-        deployedFacet.contract.address,
-        callData,
-        await getFees(maxPriorityFeePerGas)
-      );
-      await transactionResponse.wait(confirmations);
-    }
-  }
-
-  // Return an array of objects with facet name and contract properties
-  return deployedFacets;
+  // Make the deployment with generic method
+  return deployProtocolHandlerFacetsWithArgs(diamond, facetData, maxPriorityFeePerGas, doCut);
 }
 
 /**
