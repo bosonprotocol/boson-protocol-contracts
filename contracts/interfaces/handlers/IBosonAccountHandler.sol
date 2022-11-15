480fe12ecfc8cbf1faa9698ddc76188cb5a25a1d
Ana Julia <anajuliabit@gmail.com>
2022-11-15 15:38:05 -0300

    Run fix natspec interfaceId

--- contracts/interfaces/handlers/IBosonAccountHandler.sol
+++ contracts/interfaces/handlers/IBosonAccountHandler.sol
@@ -9,7 +9,7 @@ import { IBosonAccountEvents } from "../events/IBosonAccountEvents.sol";
  *
  * @notice Handles creation, update, retrieval of accounts within the protocol.
  *
- * The ERC-165 identifier for this interface is: 0xb8667cfd
+ * The ERC-165 identifier for this interface is: 0x1f891681
  */
 interface IBosonAccountHandler is IBosonAccountEvents {
     /**
