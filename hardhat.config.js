69dfe6d2f912d158d9160355e3c74c812ab97968
zajck <klemen@impressieve.com>
2022-11-23 14:09:00 +0100

    upgrade script + hardhat task

--- hardhat.config.js
+++ hardhat.config.js
@@ -59,6 +59,14 @@ task("upgrade-facets", "Upgrade existing facets, add new facets or remove existi
     await upgradeFacets(env);
   });
 
+task("upgrade-clients", "Upgrade existing clients")
+  .addOptionalParam("env", "The deployment environment")
+  .setAction(async ({ env }) => {
+    const { upgradeClients } = await lazyImport("./scripts/upgrade-clients.js");
+
+    await upgradeClients(env);
+  });
+
 task("manage-roles", "Grant or revoke access control roles")
   .addOptionalParam("env", "The deployment environment")
   .setAction(async ({ env }) => {
