61c413739fb34430ce986d1a22fbb6b5bfca373c
cliffhall <cliff@futurescale.com>
2022-12-16 09:33:20 -0500

    WIP: Raise and Escalate Dispute
    * In IBosonOrchestrationHandler.sol,
      - update interface ID

--- contracts/interfaces/handlers/IBosonOrchestrationHandler.sol
+++ contracts/interfaces/handlers/IBosonOrchestrationHandler.sol
@@ -13,7 +13,7 @@ import { IBosonBundleEvents } from "../events/IBosonBundleEvents.sol";
  *
  * @notice Combines creation of multiple entities (accounts, offers, groups, twins, bundles) in a single transaction
  *
- * The ERC-165 identifier for this interface is: 0xb698ded2
+ * The ERC-165 identifier for this interface is: 0x4e87a892
  */
 interface IBosonOrchestrationHandler is
     IBosonAccountEvents,
