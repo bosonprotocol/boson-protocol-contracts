bc9f55c1f94084952b36fed1bb26f0b6485e1b2c
Cliff Hall <cliff@futurescale.com>
2022-12-16 18:08:13 -0500

    Update contracts/protocol/facets/OrchestrationHandlerFacet.sol
    Co-authored-by: zajck <64400885+zajck@users.noreply.github.com>

--- contracts/protocol/facets/OrchestrationHandlerFacet.sol
+++ contracts/protocol/facets/OrchestrationHandlerFacet.sol
@@ -42,7 +42,7 @@ contract OrchestrationHandlerFacet is
      * buyer escalation deposit percentage of the offer price, which
      * will be added to the pot for resolution.
      *
-     * Emits a DisputeEscalated event if successful.
+     * Emits a DisputeRaised and a DisputeEscalated event if successful.
      *
      * Reverts if:
      * - The disputes region of protocol is paused
