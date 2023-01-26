065206878a00dfb672bc651266589045c3ad7638
Ana Julia <anajuliabit@gmail.com>
2023-01-26 16:28:36 -0300

    Upgrade clients to v2.2.0-rc.2 on staging and test env

--- scripts/config/client-upgrade.js
+++ scripts/config/client-upgrade.js
@@ -3,7 +3,8 @@ module.exports = {
     mainnet: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     hardhat: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     localhost: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
-    test: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
+    test: "0x69015912AA33720b842dCD6aC059Ed623F28d9f7",
+    staging: "0x69015912AA33720b842dCD6aC059Ed623F28d9f7",
     mumbai: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     polygon: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
   },
