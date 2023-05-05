d65bed2210d577df5535d61aeb90437c0fe70648
Ana Julia Bittencourt <anajuliabit@gmail.com>
2023-05-05 09:26:30 -0300

    Add upgrade tests and migrations to v2.2.1  (#621)
    * 2.2.0 mumbai test addresses
    
    * Correctly handle constructor arguments
    
    * Add 2.2.1 mumbai test addresses
    
    * Add constructor args to deployed facets json
    
    * test: fetch tag on upgrade setup
    
    * Add basic test setup version 2.2.1
    
    * Fix duplicate interface for IERC165
    
    * Fix metadataUri default to string
    
    * Add create seller tests
    
    * Refactor 00_config.js
    
    * Review changes
    
    * Refactor upgrade test
    
    * Add version upgrade test
    
    * Create 2.2.1 migration
    
    * Add updateSeller to functions hashs to update
    
    * Add tests to check isAllowlisted metaTx private state
    
    * Fix broken tests
    
    * Make old versions upgrade test compatible with new changes
    
    * Create hardhat task to migrate
    
    * Add more actions to migrate_2.2.1
    
    * Fix 2.2.1 version check test
    
    * Add manage-roles to migration
    
    * Remove Account and Orchestration handlers from facets to init
    
    * Removing timeout
    
    * Review changes
    
    * Review changes
    
    * Fix versioning inconsistencies in deployment scripts and utils
    
    * Update upgrade scripts to include newVersion parameter
    
    * Fix default version in deployAndCutFacets
    
    * add test for initV2_2_1.
    
    * Updated migrate script to reset local changes before upgrading
    
    ---------
    
    Co-authored-by: zajck <klemen@impressieve.com>
    Co-authored-by: Klemen <64400885+zajck@users.noreply.github.com>

--- scripts/util/deploy-protocol-handler-facets.js
+++ scripts/util/deploy-protocol-handler-facets.js
@@ -23,7 +23,7 @@ async function deployAndCutFacets(
   diamond,
   facetData,
   maxPriorityFeePerGas,
-  version,
+  version = "2.2.0",
   initializationFacet,
   interfacesToAdd = []
 ) {
@@ -37,7 +37,7 @@ async function deployAndCutFacets(
 
   const initializeCalldata = getInitializeCalldata(
     facetsToInit,
-    version ?? "2.0.0",
+    version,
     false,
     "0x", // no initialization data
     initializationFacet,
