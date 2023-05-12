f2a3f31cf426c37eb5617e9f9c94fde6f05769d7
zajck <klemen@impressieve.com>
2023-05-12 11:48:15 +0200

    remove unused variable - #612

--- contracts/protocol/clients/voucher/BosonVoucher.sol
+++ contracts/protocol/clients/voucher/BosonVoucher.sol
@@ -856,11 +856,4 @@ contract BosonVoucher is BosonVoucherBase, ERC2771ContextUpgradeable {
     {
         return ERC2771ContextUpgradeable._msgSender();
     }
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[50] private __gap;
 }
