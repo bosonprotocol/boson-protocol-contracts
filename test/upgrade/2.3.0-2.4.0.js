2cc1663f6253c05701dae95b0e1d73092eacc305
zajck <klemen@impressieve.com>
2024-03-04 08:42:18 +0100

    Enable back skipped tests

--- test/upgrade/2.3.0-2.4.0.js
+++ test/upgrade/2.3.0-2.4.0.js
@@ -16,7 +16,6 @@ const TokenType = require("../../scripts/domain/TokenType");
 const EvaluationMethod = require("../../scripts/domain/EvaluationMethod");
 const { RevertReasons } = require("../../scripts/config/revert-reasons");
 
-// const { getStateModifyingFunctionsHashes } = require("../../scripts/util/diamond-utils.js");
 const {
   getSnapshot,
   revertToSnapshot,
@@ -260,7 +259,7 @@ describe("[@skip-on-coverage] After facet upgrade, everything is still operation
       // Generic context needs values that are set in "before", however "before" is executed before tests, not before suites
       // and those values are undefined if this is placed outside "before".
       // Normally, this would be solved with mocha's --delay option, but it does not behave as expected when running with hardhat.
-      context.skip(
+      context(
         "Generic tests",
         getGenericContext(
           deployer,
@@ -285,7 +284,7 @@ describe("[@skip-on-coverage] After facet upgrade, everything is still operation
         _royaltyPercentage: "_royaltyPercentageUnused",
       };
 
-      context.skip(
+      context(
         "Generic tests on Voucher",
         getGenericContextVoucher(
           deployer,
