b7d839ea6cd3bcd1467ae9c979fbe8d5b2671d90
0xlucian <0xluciandev@gmail.com>
2025-04-10 15:38:32 +0300

    chore: update protocol config up until 2.4.1

--- scripts/config/protocol-parameters.js
+++ scripts/config/protocol-parameters.js
@@ -7,9 +7,11 @@ const { oneWeek, ninetyDays } = require("../../test/util/constants");
 
 module.exports = {
   // Protocol configuration params
-  protocolFeePercentage: "50", // 0.5%  : 50
-  protocolFeeFlatBoson: "0",
-  buyerEscalationDepositPercentage: "1000", // 10%,
+  fees: {
+    percentage: "50", // 0.5%  : 50
+    flatBoson: "0",
+    buyerEscalationDepositPercentage: "1000", // 10%
+  },
   limits: {
     maxExchangesPerBatch: "140",
     maxOffersPerGroup: "95",
@@ -35,15 +37,16 @@ module.exports = {
     hardhat: "0x2cDA796787425AF0892F20F6019704F053bCD6bF", //dummy
     localhost: "0x2cDA796787425AF0892F20F6019704F053bCD6bF", //dummy
     test: "0x520ce45DF6d14334257BFdD360a5C22B06E309c7", //dummy
+    mumbai: "0x1f5431E8679630790E8EbA3a9b41d1BB4d41aeD0",
     polygon: "0x9b3b0703d392321ad24338ff1f846650437a43c9",
     sepolia: "0x791Bf9Da3DEF5D7Cd3A7a748e56720Cd119D53AC",
     amoy: "0x94e32c4bfcA1D3fe08B6F8252ABB47A5B14AC2bD",
     baseSepolia: "0xd4857D5e326eee33d7bC5d2494524Dab65d55851",
-    base: "0x1111111111111111111111111111111111111111", // TODO
-    optimismSepolia: "0x1111111111111111111111111111111111111111", // TODO
-    optimism: "0x1111111111111111111111111111111111111111", // TODO
-    arbitrumSepolia: "0x1111111111111111111111111111111111111111", // TODO
-    arbitrum: "0x1111111111111111111111111111111111111111", // TODO
+    base: "0x2192607C3CBA9Ec3D490206d10d831E68E5F3c97",
+    optimismSepolia: "0xe8637906721051d860af222e6021826887d9e358",
+    optimism: "0x647fE0cCA3DF596ba414C8c600D441BB3D10d616", // TODO
+    arbitrumSepolia: "0x9aa2be49567a2c86b30c703662e376146ded9b32",
+    arbitrum: "0x54B334d68cf5382feE7FBBE496FCf1e76D9BA000",
   },
 
   // Treasury contract address
@@ -52,15 +55,16 @@ module.exports = {
     hardhat: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
     localhost: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
     test: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
+    mumbai: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
     polygon: "0x11D0d293751E18FCC56c70E1FB264CeB9f7C3fE7",
     sepolia: "0x2a91A0148EE62fA638bE38C7eE05c29a3e568dD8",
     amoy: "0x2a91A0148EE62fA638bE38C7eE05c29a3e568dD8",
     baseSepolia: "0x2a91A0148EE62fA638bE38C7eE05c29a3e568dD8",
-    base: "0x1111111111111111111111111111111111111111", // TODO
+    base: "0xA64f483e9Bb95F9FC695819c9AA8ca5D32B3cE8e",
     optimismSepolia: "0x2a91A0148EE62fA638bE38C7eE05c29a3e568dD8",
-    optimism: "0x1111111111111111111111111111111111111111", // TODO
+    optimism: "0x3B5F8C5B8112487239a51070141076e1b0eD7eeC",
     arbitrumSepolia: "0x2a91A0148EE62fA638bE38C7eE05c29a3e568dD8",
-    arbitrum: "0x1111111111111111111111111111111111111111", // TODO
+    arbitrum: "0x3B5F8C5B8112487239a51070141076e1b0eD7eeC",
   },
 
   // Boson voucher beacon contract address
@@ -69,6 +73,7 @@ module.exports = {
     hardhat: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     localhost: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     test: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
+    mumbai: "0x494f5238b40119e707582Ce87E0ca3627dB23Bcb", //dummy
     polygon: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy,
     sepolia: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
     amoy: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
@@ -86,6 +91,7 @@ module.exports = {
     hardhat: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     localhost: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     test: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
+    mumbai: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     polygon: "0x17CDD65bebDe68cd8A4045422Fcff825A0740Ef9", //dummy
     sepolia: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     amoy: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
@@ -99,6 +105,7 @@ module.exports = {
 
   EXCHANGE_ID_2_2_0: {
     hardhat: 1,
+    mumbai: 1, // test: 1, staging: 1
     amoy: 1, // test: 1, staging: 1
     polygon: 413,
     localhost: 1,
@@ -117,9 +124,10 @@ module.exports = {
     hardhat: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     localhost: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     test: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
+    mumbai: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
     polygon: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
     sepolia: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9",
-    amoy: "0x52eF3d68BaB452a294342DC3e5f464d7f610f72E",
+    amoy: "0x113d6C5038832f567808677B4F0B89ffC62c18F7",
     baseSepolia: "0x4200000000000000000000000000000000000006",
     base: "0x4200000000000000000000000000000000000006",
     optimismSepolia: "0x4200000000000000000000000000000000000006",
@@ -133,14 +141,15 @@ module.exports = {
     hardhat: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     localhost: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
     test: "0x4102621Ac55e068e148Da09151ce92102c952aab", //dummy
+    mumbai: "0x74874fF29597b6e01E16475b7BB9D6dC954d0411",
     polygon: "0xb60cf39Fb18e5111174f346d0f39521ef6531fD4",
     sepolia: "0x789d8727b9ae0A8546489232EB55b6fBE86b21Ac",
-    amoy: "0xFFcd4c407B60B0d4351945484F9354d2C9E34EA1",
+    amoy: "0xF4f02BAE43cf66fca47eBaC58657586Aa951D135",
     baseSepolia: "0xFDD51a6DB1cE50d1C33b98782035f3cB1E7E1f14",
-    base: "0x1111111111111111111111111111111111111111", // TODO
-    optimismSepolia: "0x1111111111111111111111111111111111111111", // TODO
-    optimism: "0x1111111111111111111111111111111111111111", // TODO
-    arbitrumSepolia: "0x1111111111111111111111111111111111111111", // TODO
-    arbitrum: "0x1111111111111111111111111111111111111111", // TODO
+    base: "0xd8C3B2232B3d597156148f757Bb2bd665843aABd",
+    optimismSepolia: "0x661A3e65CD3247c5EAb7f398e0617072C4Cdf19B",
+    optimism: "0xd8C3B2232B3d597156148f757Bb2bd665843aABd",
+    arbitrumSepolia: "0xFFcd4c407B60B0d4351945484F9354d2C9E34EA1",
+    arbitrum: "0xd8C3B2232B3d597156148f757Bb2bd665843aABd",
   },
 };
