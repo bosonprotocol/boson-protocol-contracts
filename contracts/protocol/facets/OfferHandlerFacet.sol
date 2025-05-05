8d74f12b8ad3c5e4ef0cf57fbacc14a05138259e
zajck <klemen@impressieve.com>
2025-05-05 11:48:24 +0200

    fix [OBE-01C]  #988 fix [OHF-01C] #989

--- contracts/protocol/facets/OfferHandlerFacet.sol
+++ contracts/protocol/facets/OfferHandlerFacet.sol
@@ -38,7 +38,6 @@ contract OfferHandlerFacet is IBosonOfferHandler, OfferBase {
      * - Resolution period is not between the minimum and the maximum resolution period
      * - Voided is set to true
      * - Available quantity is set to zero
-     * - Offer type is discovery and the price is not set to zero
      * - Dispute resolver wallet is not registered, except for absolute zero offers with unspecified dispute resolver
      * - Dispute resolver is not active, except for absolute zero offers with unspecified dispute resolver
      * - Seller is not on dispute resolver's seller allow list
@@ -91,7 +90,6 @@ contract OfferHandlerFacet is IBosonOfferHandler, OfferBase {
      *   - Resolution period is not between the minimum and the maximum resolution period
      *   - Voided is set to true
      *   - Available quantity is set to zero
-     *   - Offer type is discovery and the price is not set to zero
      *   - Dispute resolver wallet is not registered, except for absolute zero offers with unspecified dispute resolver
      *   - Dispute resolver is not active, except for absolute zero offers with unspecified dispute resolver
      *   - Seller is not on dispute resolver's seller allow list
